a332466d09026b6af0e231574e00459613d8b147
jaikrai <jaikrai0@gmail.com>
2021-08-19 01:33:17 -0400

    add new line

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { NavigationContainer } from '@react-native'
 
 export default function App() {
   return (
-    <NavigationContainer></NavigationContainer>
+    <NavigationContainer> Instagram</NavigationContainer>
   );
 }
 
