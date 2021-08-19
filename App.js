e4ba55ca2bfbe56d4475deab61f678ba36b1a3bb
jaikrai <jaikrai0@gmail.com>
2021-08-19 01:24:04 -0400

    project setup

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer></NavigationContainer>
   );
 }
 
