3c6bbcfd912d72b9f6977e9d341088e73948727c
jaikrai <jaikrai0@gmail.com>
2021-12-14 12:27:53 -0500

    fixed the update function

--- components/main/Save.js
+++ components/main/Save.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, TextInput, Image, Button } from "react-native";
+import firebase from "firebase";
+
+require("firebase/firestore");
+require("firebase/firebase-storage");
 
 export default function Save(props) {
   console.log(props.route.params.image);
   const [caption, setCaption] = useState("");
+
+  const uploadImage = async () => {
+    const uri = props.route.params.image;
+
+    const childPath = `post/${
+      firebase.auth().currentUser.uid
+    }/${Math.random.toString(36)}`;
+    console.log(childPath);
+
+    const response = await fetch(uri);
+    const blob = await response.blob();
+
+    const task = firebase.storage().ref().child(childPath).put(blob);
+
+    const taskProgress = (snapshot) => {
+      console.log(`transferred: ${snapshot.bytesTransferred}`);
+    };
+
+    const taskCompleted = () => {
+      task.snapshot.ref.getDownloadURL().then((snapshot) => {
+        console.log(snapshot);
+      });
+    };
+    const taskError = (snapshot) => {
+      console.log(snapshot);
+    };
+    task.on("state_ change", taskProgress, taskError, taskCompleted);
+  };
+
   return (
     <View>
       <Image source={{ uri: props.route.params.image }} />
@@ -11,7 +44,7 @@ export default function Save(props) {
         placeholder="Write a caption..."
         onChangeText={(caption) => setCaption(caption)}
       />
-      <Button title="Save" />
+      <Button title="Save" onPress={() => uploadImage()} />
     </View>
   );
 }
